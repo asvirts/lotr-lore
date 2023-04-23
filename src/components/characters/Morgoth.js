25264562b2f712a79c73895404bff2a23f149fe7
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-23 16:58:14 -0500

    morgoth description

--- src/components/characters/Morgoth.js
+++ src/components/characters/Morgoth.js
@@ -6,10 +6,12 @@ export default function Morgoth() {
             <Nav />
             <h3 className="detail">Morgoth/Melkor</h3>
             <img src='https://tolkiengateway.net/w/images/thumb/e/ea/Joel_Kilpatrick_-_Morgoth_and_Fingolfin.jpg/1200px-Joel_Kilpatrick_-_Morgoth_and_Fingolfin.jpg' className="por-full" alt="Morgoth" />
-            <p>Eru Ilúvatar is the supreme being in J. R. R. Tolkien's legendarium. He is the creator of all things, including the Ainur, the Valar, and the Maiar. Eru is also the source of all power and knowledge.</p>
-            <p>Eru is a benevolent being who desires only good for his creation. He is patient and forgiving, but he is also just and will not tolerate evil. Eru is a mystery to even the Valar, but he is always present and watching over his creation.</p>
-            <p>Eru is a powerful being, but he is not omnipotent. He cannot violate the free will of his creations. However, he can intervene in the world when necessary. For example, he created the Sun and Moon to light the world after Melkor had corrupted the stars.</p>
-            <p>Eru is a loving and compassionate being. He cares for all of his creation, and he is always willing to forgive those who repent. Eru is a source of hope and inspiration for all who believe in him.</p>
+            <p>Melkor was a powerful Maia, one of the Ainur, the Holy Ones who sang the Music of the Ainur with Eru Ilúvatar. He was the greatest of the Ainur in terms of his power and knowledge, but he was also the most proud and ambitious. He desired to be greater than Eru himself, and he rebelled against his creator.</p>
+            <p>Melkor's rebellion led to the first war in Arda, the world that Eru created. Melkor was defeated, but he was not destroyed. He was imprisoned in the Halls of Mandos, but he eventually escaped and returned to Middle-earth.</p>
+            <p>Melkor spent the next few thousand years corrupting Arda and its inhabitants. He created the Orcs, Trolls, and other evil creatures. He waged war on the Elves and Men. He even corrupted some of the Valar, the angelic beings who ruled Middle-earth.</p>
+            <p>Melkor's ultimate goal was to rule all of Arda. He created the One Ring in an attempt to achieve this goal. The One Ring gave Melkor great power, but it also corrupted him. He became more and more evil, and he eventually lost all sense of good and evil.</p>
+            <p>Melkor was eventually defeated by the Last Alliance of Elves and Men. He was cast into the void outside of Arda, where he will remain for all eternity.</p>
+            <p>Melkor is a complex and fascinating character. He is a symbol of the dangers of pride and ambition. He is also a reminder that even the greatest of beings can be corrupted by evil.</p>
         </>
     )
 }
