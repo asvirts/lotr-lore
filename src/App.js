17b5de26b7c363001cd42e35270d12338a7fdc44
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-23 10:06:04 -0500

    new header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Lord of the Rings Lore</h1>
       </header>
     </div>
   );
