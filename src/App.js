fb5ea492f7772c8f19b1d2787d76db2538649263
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-23 10:12:22 -0500

    added nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.jpg';
 import './App.css';
 
+// Component imports
+import Nav from './components/Nav'
+
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Nav />
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Lord of the Rings Lore</h1>
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Characters</li>
+            <li>Places</li>
+            <li>Films</li>
+        </ul>
+    )
+}
