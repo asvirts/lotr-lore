27ff2e433a99d222fe3f58fa9dc82a9c42ef66e3
Andrew Virts <96750426+asvirts@users.noreply.github.com>
2023-04-23 09:49:06 -0500

    react-router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from "react-router-dom"
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { sendToVercelAnalytics } from './vitals'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
